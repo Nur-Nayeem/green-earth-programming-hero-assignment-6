3c1d377301aa1d1bfd6cfaddcc385147237c7ae8
nur-nayeem <nurnayem768@gmail.com>
2025-09-07 00:23:40 +0600

    added category section

---
+++ script.js
@@ -0,0 +1,24 @@
+const categoryList = document.getElementById("category-list")
+
+
+const loadCategory = () => {
+    fetch("https://openapi.programming-hero.com/api/categories")
+        .then(res => res.json())
+        .then(category => {
+            displayCategory(category.categories)
+        }
+        )
+}
+loadCategory()
+
+const displayCategory = (categories) => {
+    categories.forEach(cat => {
+        categoryList.innerHTML += `
+            <li id="cat-${cat.id}" class="p-2 cursor-pointer hover:bg-[var(--primary)] hover:text-white">${cat.category_name}</li>
+        `
+        console.log(cat);
+
+    });
+}
+
+
